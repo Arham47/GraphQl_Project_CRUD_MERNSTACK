5927d8eb9d95ce646be3aa1103290a46a37b24b8
Arham Khan <81346957+Arham47@users.noreply.github.com>
2023-07-28 15:46:16 +0500

    project query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,37 +1,58 @@
-import { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema ,GraphQLList} from "graphql"
+import {
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull,
+    GraphQLEnumType,
+} from "graphql";
 // mongoose model
-import ClientModel from "../models/Client.js" 
-import projectModel from "../models/Project.js" 
-
+import ClientModel from "../models/Client.js";
+import projectModel from "../models/Project.js";
 
 //client type
 
 const ClientType = new GraphQLObjectType({
     name: "Client",
     fields: () => ({
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        description: { type: GraphQLString },
-        status: { type: GraphQLString },
-        
-    })
+        id: {
+            type: GraphQLID
+        },
+        name: {
+            type: GraphQLString
+        },
+        email: {
+            type: GraphQLString
+        },
+        phone: {
+            type: GraphQLString
+        },
+    }),
 });
 const ProjectType = new GraphQLObjectType({
     name: "Project",
     fields: () => ({
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        email: { type: GraphQLString },
-        phone: { type: GraphQLString },
-        clients: {
+        id: {
+            type: GraphQLID
+        },
+        name: {
+            type: GraphQLString
+        },
+        description: {
+            type: GraphQLString
+        },
+        status: {
+            type: GraphQLString
+        },
+        client: {
             type: ClientType,
             resolve(parent, args) {
-                return ClientModel.findById(parent.clinetId)
-            }
-        }
-    })
+                return ClientModel.findById(parent.clientId);
+            },
+        },
+    }),
 });
-
 
 const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
@@ -41,36 +62,123 @@ const RootQuery = new GraphQLObjectType({
             type: new GraphQLList(ClientType),
             resolve(parent, args) {
                 return ClientModel.find();
-            }
+            },
         },
         client: {
             type: ClientType,
-            args: { id: { type: GraphQLID } },
+            args: {
+                id: {
+                    type: GraphQLID
+                }
+            },
             resolve(parent, args) {
-                return ClientModel.findById(args.id)
-            }
-            
+                return ClientModel.findById(args.id);
+            },
         },
         projects: {
             type: new GraphQLList(ProjectType),
             resolve(parent, args) {
-                return projectModel.find()
-            }
+                return projectModel.find();
+            },
         },
         project: {
             type: ProjectType,
-            args: { id: { type: GraphQLID } },
+            args: {
+                id: {
+                    type: GraphQLID
+                }
+            },
             resolve(parent, args) {
-                return projectModel.findById(args.id)
+                return projectModel.findById(args.id);
+            },
+        },
+    },
+});
+
+const mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        addClient: {
+            type: ClientType,
+            args: {
+                name: {
+                    type: GraphQLNonNull(GraphQLString)
+                },
+                email: {
+                    type: GraphQLNonNull(GraphQLString)
+                },
+                phone: {
+                    type: GraphQLNonNull(GraphQLString)
+                },
+            },
+            resolve(parent, args) {
+                const client = new ClientModel({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone,
+                });
+                return client.save();
+            },
+        },
+
+        // delete Client
+        deleteClient: {
+            type: ClientType,
+            args: {
+                id: {
+                    type: GraphQLNonNull(GraphQLID)
+                }
+            },
+            resolve(parent, args) {
+                return ClientModel.findByIdAndDelete(args.id)
             }
-            
-        }
-    }
-    
-})
 
+        },
 
+        addProject: {
+            type: ProjectType,
+            args: {
+                name: {
+                    type: GraphQLNonNull(GraphQLString)
+                },
+                description: {
+                    type: GraphQLNonNull(GraphQLString)
+                },
+                status: {
+                    type: new GraphQLEnumType({
+                        name: "ProjectStatus",
+                        values: {
+                            'new': {
+                                value: 'Not Started'
+                            },
+                            'progress': {
+                                value: 'In Progress'
+                            },
+                            'completed': {
+                                value: 'Completed'
+                            }
+                        },
+                    }),
+                    defaultValue: "Not Started"
+                },
+                clientId: {
+                    type: GraphQLNonNull(GraphQLID)
+                }
+            },
+            resolve(parent, args) {
+                const client = new projectModel({
+                    name: args.name,
+                    description: args.description,
+                    status: args.status,
+                    clientId: args.clientId,
+                });
+                return client.save();
+            },
+        },
+    },
+});
 
-export const schema=new GraphQLSchema({
-    query:RootQuery
-})
+export const schema = new GraphQLSchema({
+    query: RootQuery,
+    mutation: mutation,
+});
